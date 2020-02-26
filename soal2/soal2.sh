a8fcb413f8983d8625e063aeeaedecaeae2502cb
nashirat <55182333+nashirat@users.noreply.github.com>
2020-02-26 16:01:06 +0700

    Revisi

--- soal2/soal2.sh
+++ soal2/soal2.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 wd=`pwd`
-pass="`cat /dev/urandom | tr -cd 'a-zA-Z0-9' | fold -w 28 | head -n 1`"
+
+choose() { echo ${1:RANDOM%${#1}:1} $RANDOM; }
+pass="$({
+for i in $0
+do
+  choose '0123456789'
+  choose 'abcdefghijklmnopqrstuvwxyz'
+  choose 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+done
+}| sort -R | awk '{printf "%s" ,$1}' | cut -d " " -f 2 | fold -w 28 | head -n 1)"
+
 name="`echo $1 | tr -cd 'a-zA-Z'`"
 echo $pass > $wd/$name.txt
 namesum="`md5sum $name.txt | cut -d ' ' -f 1`"
